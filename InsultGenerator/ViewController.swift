94255d896723a36a6031fdb708dec14371b6dab0
Viktor Churikov <scanlave@gmail.com>
2021-09-26 22:03:52 +0800

    Initial Commit

---
+++ InsultGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InsultGenerator
+//
+//  Created by Виктор Чуриков on 26.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
